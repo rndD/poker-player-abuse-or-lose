275642be4adf1702dd64e506b1939822065b23d3
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-09-27 10:46:50 +0300

    Version

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 
-  VERSION: "Default JavaScript folding player",
+  VERSION: "0.1.0",
 
   bet_request: function(game_state) {
       console.log(require('util').inpspect(game_state)))
