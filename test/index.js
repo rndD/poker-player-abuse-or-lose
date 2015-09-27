7351fe831ee67014cdf5fa4c56a871829e2c0a72
alex-k <alex-k@yandex-team.ru>
2015-09-27 12:50:43 +0300

    base test

--- test/index.js
+++ test/index.js
@@ -8,6 +8,14 @@ describe('Poker', function () {
         simpleFlopGameState = require('./test_data1.js').simpleFlop;
     });
 
+    describe('base', function () {
+        it('player should', function () {
+            var bet = player.bet_request(simpleFlopGameState);
+            expect(bet).to.eql(120);
+
+        });
+    });
+
     describe('bet', function () {
         it('getMaxBetFromState', function () {
             var bet = player.getMaxBetFromState(simpleFlopGameState);
