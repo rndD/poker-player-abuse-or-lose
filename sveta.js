5d821d4177b8298be46edac2f7a0594c18d6dcfe
DH <allah.taskbar@gmail.com>
2015-09-27 15:08:49 +0400

    sveta 2

--- sveta.js
+++ sveta.js
@@ -1,14 +1,62 @@
 var sveta = function(hand, turn) {
-    if (hand.four || hand.straight_flush || hand.full_house) {
-        return 8;
+    switch(turn) {
+        case 2: return twoCard(hand);
+        case 5: return fiveCard(hand);
+        case 6: return sixCard(hand);
+        case 7: return sevenCard(hand);
     }
 
-    if (hand.flush || hand.straight || hand.three || hand.two) {
+    console.log('&&SVETA ERROR');
+    return twoCard(hand);
+};
+
+function twoCard(hand) {
+    if (hand.two) {
         return 4;
     }
 
     else return 0;
-};
+}
+
+function fiveCard(hand) {
+    if (hand.four || hand.straight_flush || hand.full_house || hand.flush) {
+        return 8;
+    }
+
+    if (hand.straight || hand.three || hand.two) {
+        return 4;
+    }
+
+    else return 0;
+}
+
+function sixCard(hand) {
+    if (hand.four || hand.straight_flush || hand.full_house || hand.flush) {
+        return 8;
+    }
+
+    if (hand.straight || hand.three || hand.two) {
+        return 4;
+    }
+
+    else return 0;
+}
+
+function sevenCard(hand) {
+    if (hand.four || hand.straight_flush || hand.full_house || hand.flush) {
+        return 8;
+    }
+
+    if (hand.straight || hand.three) {
+        return 4;
+    }
+
+    else if (hand.two) {
+        return 0;
+    }
+
+    else return 0;
+}
 
 module.exports = sveta;
 
