d36258f77aec8ff15fa5139cb52c40fd946aee69
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2015-09-27 10:40:36 +0300

    add console.log

--- player.js
+++ player.js
@@ -4,6 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
+      console.log(require('util').inpspect(game_state)))
     return 40;
   },
 
